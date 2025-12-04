f7f29365c7f5fa953ab05504ecc79fe65ec7f3c9
Rafael Bento <rafael.bentoe.silva@gmail.com>
2025-12-04 17:21:14 -0300

    Remove some unit tests

--- genkit-angular-starter-kit/src/app/app.component.spec.ts
+++ genkit-angular-starter-kit/src/app/app.component.spec.ts
@@ -20,17 +20,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'genkit-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('genkit-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('div')?.className).toEqual('app-container');
-  });
 });
